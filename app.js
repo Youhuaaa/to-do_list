6f1940d412c6f0170a084c70302538d53fc86c07
Youhuaaa <25jasofer@gmail.com>
2022-08-10 14:15:56 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const port = 3000
 
 const exphbs = require('express-handlebars')
 
+// connect to database MongoDB vis mongoose
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://youhuaaa:8EeNc5NyFn@cluster0.9pcddue.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected')
+})
+
 app.get('/', (req, res) => {
   res.send('Initialize /project')
 })
